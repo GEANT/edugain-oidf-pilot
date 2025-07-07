4a85e288fc6089303d2563a700b4828a71d9c4a8
Davide Vaghetti <davide.vaghetti@garr.it>
2025-07-07 17:54:49 +0200

    Initial commit

---
+++ pilot/stop-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd rp1 && docker compose down && cd ..
+cd ia1 && docker compose down && cd ..
+cd ia2 && docker compose down && cd ..
+cd ta && docker compose down && cd ..
+cd caddy && docker compose down && cd ..
