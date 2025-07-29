ea0f4c03613f9ae084de4d8e058dba0c12ca39a4
Davide Vaghetti <davide.vaghetti@garr.it>
2025-07-29 10:12:06 +0200

    Initial commit with no history

---
+++ pilot/start-all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+cd caddy && docker compose up -d && cd ..
+cd ta && docker compose up -d && cd ..
+cd ia1 && docker compose up -d && cd ..
+cd ia2 && docker compose up -d && cd ..
+cd rp1 && docker compose up -d && cd ..
